b283f9d6bb75cbded80c8dc6bd11e928c99e2f1a
codexshell <ericoderito@gmail.com>
2023-02-13 20:00:40 +0300

    fix: add padding to main section of homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,42 +10,44 @@ onMounted(() => console.log(video!.value!.play()));
 </script>
 
 <template>
-  <span class="mt-[48px] mr-[40px] flex gap-4">
-    <span><img :src="logoUrl" alt="" /></span>
-    <span
-      class="font-brand-1 text-[20px] font-bold leading-[25px] tracking-[-0.16em]"
-      >Dreamshow</span
-    >
-  </span>
-  <header class="mt-20">
-    <h1
-      class="mx-auto w-[618px] text-center font-brand-2 text-[48px] font-bold leading-[64px]"
-    >
-      Create your own AI apps <span class="text-brand-blue">in minutes.</span>
-    </h1>
-    <p
-      class="mx-auto mt-8 text-center font-brand-2 text-[24px] font-medium leading-[36px]"
-    >
-      With Dreamshow, you can turn your knowledge into a<br />
-      an AI app in minutes. Bring your ideas to life today.
-    </p>
-  </header>
-  <main>
-    <router-link :to="{ name: 'experience' }">
-      <button
-        class="mx-auto mt-6 block h-[52px] w-[200px] rounded-[16px] bg-brand-blue text-center font-brand-2 font-bold leading-[36px] text-brand-white"
+  <div class="pb-32">
+    <span class="mt-[48px] mr-[40px] flex gap-4">
+      <span><img :src="logoUrl" alt="" /></span>
+      <span
+        class="font-brand-1 text-[20px] font-bold leading-[25px] tracking-[-0.16em]"
+        >Dreamshow</span
       >
-        early access
-      </button>
-    </router-link>
+    </span>
+    <header class="mt-20">
+      <h1
+        class="mx-auto w-[618px] text-center font-brand-2 text-[48px] font-bold leading-[64px]"
+      >
+        Create your own AI apps <span class="text-brand-blue">in minutes.</span>
+      </h1>
+      <p
+        class="mx-auto mt-8 text-center font-brand-2 text-[24px] font-medium leading-[36px]"
+      >
+        With Dreamshow, you can turn your knowledge into a<br />
+        an AI app in minutes. Bring your ideas to life today.
+      </p>
+    </header>
+    <main>
+      <router-link :to="{ name: 'experience' }">
+        <button
+          class="mx-auto mt-6 block h-[52px] w-[200px] rounded-[16px] bg-brand-blue text-center font-brand-2 font-bold leading-[36px] text-brand-white"
+        >
+          early access
+        </button>
+      </router-link>
 
-    <video
-      ref="video"
-      width="1000"
-      autoplay
-      loop
-      :src="videoUrl"
-      class="mx-auto mt-16"
-    ></video>
-  </main>
+      <video
+        ref="video"
+        width="1000"
+        autoplay
+        loop
+        :src="videoUrl"
+        class="mx-auto mt-16"
+      ></video>
+    </main>
+  </div>
 </template>
